45faa359e8bf19fab0c15afb6cc09b7c31cd99ab
mgt <mgt@oi-wiki.org>
2023-04-10 22:17:45 +0800

    add preview on type

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { spawn } from 'child_process';
-import { readFileSync } from 'fs'
+import { readFile } from 'fs/promises';
+import * as path from 'path';
 
-function loadHTMLFile(context: vscode.ExtensionContext, relativePath: string) {
+async function loadHTMLFile(context: vscode.ExtensionContext, relativePath: string) {
 	const fileUri = vscode.Uri.joinPath(context.extensionUri, relativePath);
-	const fileContents = readFileSync(fileUri.fsPath, 'utf8');
+	const fileContents = await readFile(fileUri.fsPath, 'utf8');
 	return fileContents;
 }
 
@@ -25,14 +26,34 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('typst-preview.preview', () => {
+	let disposable = vscode.commands.registerCommand('typst-preview.preview', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		let activeEditor = vscode.window.activeTextEditor;
 		if (activeEditor) {
 			let filePath = activeEditor.document.uri.fsPath;
 			console.log('File path:', filePath);
-			const serverProcess = spawn(getTypstWsPath(), ['watch', filePath]);
+			// get file dir using path
+			let rootDir = path.dirname(filePath);
+			let filename = path.basename(filePath);
+			let shadowFilePath = path.join(rootDir, '.typst-preview.' + filename);
+			console.log('shadow file path:', shadowFilePath);
+			// copy file content to shadow file
+			let fileContent = activeEditor.document.getText();
+			await vscode.workspace.fs.writeFile(vscode.Uri.file(shadowFilePath), Buffer.from(fileContent));
+			const update = async () => {
+				// save file content to shadow file
+				if (activeEditor?.document) {
+					let fileContent = activeEditor?.document.getText();
+					await vscode.workspace.fs.writeFile(vscode.Uri.file(shadowFilePath), Buffer.from(fileContent));
+				}
+			};
+			vscode.workspace.onDidChangeTextDocument(async (e) => {
+				if (e.document === activeEditor?.document) {
+					await update();
+				}
+			});
+			const serverProcess = spawn(getTypstWsPath(), ['watch', shadowFilePath]);
 			serverProcess.stdout.on('data', (data) => {
 				console.log(`${data}`);
 			});
@@ -44,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
 			serverProcess.on('exit', (code) => {
 				console.log(`child process exited with code ${code}`);
 			});
-			console.log('Launched server')
+			console.log('Launched server');
 			const webviewOptions = {
 				enableScripts: true,
 				enableWebsockets: true,
@@ -56,21 +77,23 @@ export function activate(context: vscode.ExtensionContext) {
 				'Preview', // 面板标题
 				vscode.ViewColumn.Beside, // 显示在编辑器的哪一侧
 				{
-					enableScripts: true, // 启用JS
+					enableScripts: true, // 启用 JS
 				}
 			);
 
-			panel.onDidDispose(() => {
-				serverProcess.kill()
-				panel.dispose()
-				console.log("killing preview service")
-			})
+			panel.onDidDispose(async () => {
+				serverProcess.kill();
+				panel.dispose();
+				// remove shadow file
+				await vscode.workspace.fs.delete(vscode.Uri.file(shadowFilePath));
+				console.log("killing preview service");
+			});
 
-			// 将已经准备好的HTML设置为Webview内容
-			const html = loadHTMLFile(context, './index.html');
-			panel.webview.html = html
+			// 将已经准备好的 HTML 设置为 Webview 内容
+			const html = await loadHTMLFile(context, './index.html');
+			panel.webview.html = html;
 		} else {
-			console.log('No active editor');
+			vscode.window.showWarningMessage('No active editor');
 		}
 	});
 
@@ -78,4 +101,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
