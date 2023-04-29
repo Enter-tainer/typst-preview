0afa2db7597834c284efcc233c371af923519640
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-29 17:38:04 +0800

    test: test function of font path configuration

--- addons/vscode/src/test/suite/extension.test.ts
+++ addons/vscode/src/test/suite/extension.test.ts
@@ -3,7 +3,10 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as ext from '../../extension';
+
+const jsonIs = (pred: (x: string, y: string) => void) => (x: unknown, y: unknown) =>
+  pred(JSON.stringify(x), JSON.stringify(y));
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -14,6 +17,26 @@ suite('Extension Test Suite', () => {
 	});
 
 	test('Executable Configuration Test', () => {
-		assert.strictEqual('typst-ws', vscode.workspace.getConfiguration().get<string>('typst-ws.executable'));
+
+  test("FontPaths Configuration Test", async () => {
+
+    /// check that default font paths should be []
+    jsonIs(assert.strictEqual)(
+      [],
+      vscode.workspace.getConfiguration().get<string[]>("typst-preview.font-paths")
+    );
+
+    jsonIs(assert.strictEqual)(
+      [], ext.getTypstWsFontArgs(undefined));
+
+    jsonIs(assert.strictEqual)(
+      [], ext.getTypstWsFontArgs([]));
+
+    jsonIs(assert.strictEqual)(
+      [], ext.codeGetTypstWsFontArgs());
+
+    jsonIs(assert.strictEqual)(
+      ["--font-path", "/path/to/font1", "--font-path", "/path/to/font2"], 
+      ext.getTypstWsFontArgs(["/path/to/font1", "/path/to/font2"]));
 	});
 });
