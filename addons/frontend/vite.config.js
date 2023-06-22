24371d3830fffd96fa804bb0b086f8329c272c22
Myriad-Dreamin <camiyoru@gmail.com>
2023-06-22 11:16:02 +0800

    Bundle inputs together

--- addons/frontend/vite.config.js
+++ addons/frontend/vite.config.js
@@ -4,11 +4,6 @@ export default defineConfig({
   build: {
     minify: false,
     rollupOptions: {
-      input: {
-        app: "index.html",
-        main: "src/main.js",
-        svg: "src/svg.ts",
-      },
       output: {
         assetFileNames: `typst-webview-assets/[name]-[hash][extname]`,
         chunkFileNames: "typst-webview-assets/[name]-[hash].js",
