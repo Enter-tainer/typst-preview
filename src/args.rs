597e7ca5448febfa97ed25cfdf5c591a1b04f520
mgt <mgt@oi-wiki.org>
2023-08-06 17:22:15 +0800

    use comment string

--- src/args.rs
+++ src/args.rs
@@ -9,25 +9,28 @@ pub struct CliArguments {
     #[clap(long = "font-path", value_name = "DIR", action = ArgAction::Append)]
     pub font_paths: Vec<PathBuf>,
 
-    /// Configure the root for absolute paths
-    #[clap(long = "root", value_name = "DIR", help = "root directory for your project")]
+    /// Root directory for your project
+    #[clap(long = "root", value_name = "DIR")]
     pub root: Option<PathBuf>,
 
-    /// Configure the websocket path
-    #[clap(long = "data-plane-host", default_value = "127.0.0.1:23625", value_name = "HOST", help = "data plane server will bind to this address")]
+    /// Data plane server will bind to this address
+    #[clap(long = "data-plane-host", default_value = "127.0.0.1:23625", value_name = "HOST")]
     pub data_plane_host: String,
 
-    /// Configure the websocket path
-    #[clap(long = "control-plane-host", default_value = "127.0.0.1:23626", value_name = "HOST", help = "control plane server will bind to this address")]
+    /// Control plane server will bind to this address
+    #[clap(long = "control-plane-host", default_value = "127.0.0.1:23626", value_name = "HOST")]
     pub control_plane_host: Option<String>,
 
-    #[clap(long = "open-in-browser-host", help = "Host to open the preview in the browser.", value_name = "HOST", default_value = "127.0.0.1:23267")]
+    /// Host to open the preview in the browser.
+    #[clap(long = "open-in-browser-host", value_name = "HOST", default_value = "127.0.0.1:23267")]
     pub open_in_browser_host: Option<String>,
 
-    #[clap(long = "open-in-browser", help = "Open the preview in the browser after compilation.")]
+    /// Open the preview in the browser after compilation.
+    #[clap(long = "open-in-browser")]
     pub open_in_browser: bool,
 
-    #[clap(long = "partial-rendering", help = "Only render visible part of the document. This can improve performance but still being experimental.")]
+    /// Only render visible part of the document. This can improve performance but still being experimental.
+    #[clap(long = "partial-rendering")]
     pub enable_partial_rendering: bool,
     
     pub input: PathBuf,
