2328b08f9ee19676703c1985e6405cadaf98b843
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-10-15 21:03:03 +0800

    dev: use upstream watch & notify actor (#117)
    Co-authored-by: mgt <mgt@oi-wiki.org>

--- src/actor/typst.rs
+++ src/actor/typst.rs
@@ -1,18 +1,12 @@
-use std::{path::Path, sync::Arc};
+use std::sync::Arc;
 
-use crate::{
-    jump_from_cursor, DocToSrcJumpInfo, MemoryFiles, MemoryFilesShort, SrcToDocJumpRequest,
-};
+use crate::{MemoryFiles, MemoryFilesShort, SrcToDocJumpRequest};
 use log::{debug, error, info};
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use tokio::sync::{broadcast, mpsc, watch};
-use typst::syntax::{FileId, VirtualPath};
 use typst::{doc::Document, World};
-use typst_ts_compiler::{
-    service::{CompileDriver, Compiler, DiagObserver},
-    ShadowApi,
-};
-use typst_ts_core::vector::span_id_from_u64;
+use typst_ts_compiler::service::CompileDriver;
+use typst_ts_compiler::service::{CompileActor, CompileClient as TsCompileClient, CompileExporter};
+use typst_ts_compiler::vfs::notify::{FileChangeSet, MemoryEvent};
 
 use super::render::RenderActorRequest;
 use super::{
@@ -28,19 +22,14 @@ pub enum TypstActorRequest {
     SyncMemoryFiles(MemoryFiles),
     UpdateMemoryFiles(MemoryFiles),
     RemoveMemoryFiles(MemoryFilesShort),
-
-    FilesystemEvent(notify::Event),
 }
 
-pub struct TypstActor {
-    compiler_driver: CompileDriver,
-    fs_watcher: RecommendedWatcher,
-    mailbox: mpsc::UnboundedReceiver<TypstActorRequest>,
+type CompileService = CompileActor<CompileExporter<CompileDriver>>;
+type CompileClient = TsCompileClient<CompileService>;
 
-    doc_sender: watch::Sender<Option<Arc<Document>>>,
-    renderer_sender: broadcast::Sender<RenderActorRequest>,
-    doc_to_src_jump_sender: mpsc::UnboundedSender<EditorActorRequest>,
-    src_to_doc_jump_sender: broadcast::Sender<WebviewActorRequest>,
+pub struct TypstActor {
+    inner: CompileService,
+    client: TypstClient,
 }
 
 type MpScChannel<T> = (mpsc::UnboundedSender<T>, mpsc::UnboundedReceiver<T>);
@@ -70,88 +59,109 @@ impl TypstActor {
             src_to_doc_jump,
         }
     }
+
     pub fn new(
         compiler_driver: CompileDriver,
         mailbox: mpsc::UnboundedReceiver<TypstActorRequest>,
-        mailbox_sender: mpsc::UnboundedSender<TypstActorRequest>,
         doc_sender: watch::Sender<Option<Arc<Document>>>,
         renderer_sender: broadcast::Sender<RenderActorRequest>,
         doc_to_src_jump_sender: mpsc::UnboundedSender<EditorActorRequest>,
         src_to_doc_jump_sender: broadcast::Sender<WebviewActorRequest>,
     ) -> Self {
-        let fs_watcher = RecommendedWatcher::new(
-            move |res: Result<notify::Event, _>| match res {
-                Ok(e) => {
-                    info!("TypstActor: filesystem event: {:?}", e);
-                    mailbox_sender
-                        .send(TypstActorRequest::FilesystemEvent(e))
-                        .unwrap();
-                }
-                Err(e) => error!("watch error: {:#}", e),
+        // CompileExporter + DynamicLayoutCompiler + WatchDriver
+        let root = compiler_driver.world.root.clone();
+        let driver = CompileExporter::new(compiler_driver).with_exporter(
+            move |_world: &dyn World, doc: Arc<Document>| {
+                let _ = doc_sender.send(Some(doc)); // it is ok to ignore the error here
+                let _ = renderer_sender.send(RenderActorRequest::RenderIncremental);
+                Ok(())
             },
-            notify::Config::default(),
         );
-        let fs_watcher = match fs_watcher {
-            Ok(w) => w,
-            Err(e) => {
-                error!("TypstActor: failed to create filesystem watcher: {:#}", e);
-                panic!();
-            }
-        };
+        let inner = CompileActor::new(driver, root.as_ref().to_owned()).with_watch(true);
 
         Self {
-            compiler_driver,
-            fs_watcher,
-            mailbox,
-            doc_sender,
-            renderer_sender,
-            doc_to_src_jump_sender,
-            src_to_doc_jump_sender,
+            inner,
+            client: TypstClient {
+                inner: once_cell::sync::OnceCell::new(),
+                mailbox,
+                doc_to_src_jump_sender,
+                src_to_doc_jump_sender,
+            },
         }
     }
-    pub fn run(mut self) {
-        self.compile();
-        let Ok(_) = self
-            .fs_watcher
-            .watch(&self.compiler_driver.world.root, RecursiveMode::Recursive)
-        else {
-            error!("TypstActor: failed to watch filesystem events");
-            panic!();
-        };
-        loop {
-            let mut recompile = false;
-            debug!("TypstActor: waiting for message");
-            let Some(mail) = self.mailbox.blocking_recv() else {
-                info!("TypstActor: no more messages");
-                break;
-            };
-            recompile |= self.need_recompile(&mail);
-            self.process_mail(mail);
-            // read the queue to empty
-            while let Ok(mail) = self.mailbox.try_recv() {
-                recompile |= self.need_recompile(&mail);
-                self.process_mail(mail);
-            }
-            if recompile {
-                self.compile();
-            }
+
+    pub async fn run(self) {
+        let (server, client) = self.inner.split();
+        server.spawn().await;
+
+        if self.client.inner.set(client).is_err() {
+            unreachable!();
+        }
+
+        let mut client = self.client;
+
+        debug!("TypstActor: waiting for message");
+        while let Some(mail) = client.mailbox.recv().await {
+            client.process_mail(mail).await;
         }
         info!("TypstActor: exiting");
     }
+}
 
-    fn process_mail(&mut self, mail: TypstActorRequest) {
-        match &mail {
+struct TypstClient {
+    inner: once_cell::sync::OnceCell<CompileClient>,
+
+    mailbox: mpsc::UnboundedReceiver<TypstActorRequest>,
+
+    doc_to_src_jump_sender: mpsc::UnboundedSender<EditorActorRequest>,
+    src_to_doc_jump_sender: broadcast::Sender<WebviewActorRequest>,
+}
+
+impl TypstClient {
+    fn inner(&mut self) -> &mut CompileClient {
+        self.inner.get_mut().unwrap()
+    }
+
+    async fn process_mail(&mut self, mail: TypstActorRequest) {
+        match mail {
             TypstActorRequest::DocToSrcJumpResolve(id) => {
-                debug!("TypstActor: processing message: {:?}", mail);
-                if let Some(info) = self.resolve_doc_to_src_jump(*id) {
+                debug!("TypstActor: processing doc2src: {:?}", id);
+
+                let res = self
+                    .inner()
+                    .resolve_doc_to_src_jump(id)
+                    .await
+                    .map_err(|err| {
+                        error!("TypstActor: failed to resolve doc to src jump: {:#}", err);
+                    })
+                    .ok()
+                    .flatten();
+
+                if let Some(info) = res {
                     let _ = self
                         .doc_to_src_jump_sender
                         .send(EditorActorRequest::DocToSrcJump(info));
                 }
             }
             TypstActorRequest::SrcToDocJumpResolve(req) => {
-                debug!("TypstActor: processing message: {:?}", mail);
-                if let Some(info) = self.resolve_src_to_doc_jump(req) {
+                debug!("TypstActor: processing src2doc: {:?}", req);
+
+                let res = self
+                    .inner()
+                    .resolve_src_to_doc_jump(req.filepath, req.line, req.character)
+                    .await
+                    .map_err(|err| {
+                        error!("TypstActor: failed to resolve src to doc jump: {:#}", err);
+                    })
+                    .ok()
+                    .flatten()
+                    .map(|jump_pos| SrcToDocJumpInfo {
+                        page_no: jump_pos.page.into(),
+                        x: jump_pos.point.x.to_pt(),
+                        y: jump_pos.point.y.to_pt(),
+                    });
+
+                if let Some(info) = res {
                     let _ = self
                         .src_to_doc_jump_sender
                         .send(WebviewActorRequest::SrcToDocJump(info));
@@ -175,98 +185,33 @@ impl TypstActor {
                 debug!("TypstActor: processing REMOVE memory files: {:?}", m.files);
                 self.remove_shadow_files(m);
             }
-            TypstActorRequest::FilesystemEvent(_e) => {}
         }
     }
 
-    fn compile(&mut self) {
-        self.compiler_driver.world.reset();
-        if let Some(doc) = self
-            .compiler_driver
-            .with_compile_diag::<true, _>(Compiler::compile)
-        {
-            let _ = self.doc_sender.send(Some(Arc::new(doc))); // it is ok to ignore the error here
-            let _ = self
-                .renderer_sender
-                .send(RenderActorRequest::RenderIncremental);
-            comemo::evict(30);
-        }
+    fn update_memory_files(&mut self, files: MemoryFiles, reset_shadow: bool) {
+        // todo: is it safe to believe that the path is normalized?
+        let now = std::time::SystemTime::now();
+        let files = FileChangeSet::new_inserts(
+            files
+                .files
+                .into_iter()
+                .map(|(path, content)| {
+                    let content = content.as_bytes().into();
+                    // todo: cloning PathBuf -> Arc<Path>
+                    (path.into(), Ok((now, content)).into())
+                })
+                .collect(),
+        );
+        self.inner().add_memory_changes(if reset_shadow {
+            MemoryEvent::Sync(files)
+        } else {
+            MemoryEvent::Update(files)
+        });
     }
 
-    fn need_recompile(&self, mail: &TypstActorRequest) -> bool {
-        match mail {
-            TypstActorRequest::DocToSrcJumpResolve(_)
-            | TypstActorRequest::SrcToDocJumpResolve(_) => false,
-            TypstActorRequest::SyncMemoryFiles(_)
-            | TypstActorRequest::UpdateMemoryFiles(_)
-            | TypstActorRequest::RemoveMemoryFiles(_) => true,
-            TypstActorRequest::FilesystemEvent(e) => self.compiler_driver.relevant(e),
-        }
-    }
-
-    fn update_memory_files(&mut self, files: &MemoryFiles, reset_shadow: bool) {
-        if reset_shadow {
-            self.compiler_driver.world.reset_shadow();
-        }
-        for (path, content) in files.files.iter() {
-            let path = Path::new(path).to_owned();
-            // todo: is it safe to believe that the path is normalized?
-            match self
-                .compiler_driver
-                .world
-                .map_shadow(&path, content.as_bytes().into())
-            {
-                Ok(_) => {}
-                Err(e) => {
-                    error!(
-                        "TypstActor: failed to resolve file: {}, error: {e}",
-                        path.display()
-                    );
-                    return;
-                }
-            };
-        }
-    }
-
-    fn remove_shadow_files(&mut self, files: &MemoryFilesShort) {
-        for path in files.files.iter() {
-            let path = Path::new(path);
-            // todo: ignoring the error here
-            let _ = self.compiler_driver.world.unmap_shadow(path);
-        }
-    }
-
-    fn resolve_src_to_doc_jump(&self, req: &SrcToDocJumpRequest) -> Option<SrcToDocJumpInfo> {
-        let world = &self.compiler_driver.world;
-        let relative_path = Path::new(&req.filepath).strip_prefix(&world.root).ok()?;
-        let source_id = FileId::new(None, VirtualPath::new(relative_path));
-        let source = world.source(source_id).ok()?;
-        let cursor = req.to_byte_offset(&source)?;
-        let doc = self.doc_sender.borrow().clone()?;
-        let jump_pos = jump_from_cursor(&doc.pages, &source, cursor)?;
-        Some(SrcToDocJumpInfo {
-            page_no: jump_pos.page.into(),
-            x: jump_pos.point.x.to_pt(),
-            y: jump_pos.point.y.to_pt(),
-        })
-    }
-
-    fn resolve_doc_to_src_jump(&self, id: u64) -> Option<DocToSrcJumpInfo> {
-        let span = span_id_from_u64(id)?;
-        let src_id = span.id()?;
-        let source = self.compiler_driver.world.source(src_id).ok()?;
-        let range = source.find(span)?.range();
-        let filepath = self.compiler_driver.world.path_for_id(src_id).ok()?;
-        Some(DocToSrcJumpInfo::from_option(
-            filepath.to_string_lossy().to_string(),
-            (
-                source.byte_to_line(range.start),
-                source.byte_to_column(range.start),
-            ),
-            (
-                source.byte_to_line(range.end),
-                source.byte_to_column(range.end),
-            ),
-        ))
+    fn remove_shadow_files(&mut self, files: MemoryFilesShort) {
+        // todo: is it safe to believe that the path is normalized?
+        let files = FileChangeSet::new_removes(files.files.into_iter().map(From::from).collect());
+        self.inner().add_memory_changes(MemoryEvent::Update(files))
     }
 }
