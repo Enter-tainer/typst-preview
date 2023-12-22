e31b29cd302262693d7c7fc6c0d2354bf3ed7f81
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-12-22 13:15:18 +0800

    dev: go first step to make typst-preview as a library (#202)
    * dev: split preview function
    
    * dev: make preview library
    
    * dev: decouple hyper

--- src/args.rs
+++ src/args.rs
@@ -18,20 +18,7 @@ pub enum PreviewMode {
 const ENV_PATH_SEP: char = if cfg!(windows) { ';' } else { ':' };
 
 #[derive(Debug, Clone, Parser)]
-#[clap(name = "typst-preview", author, version, about, long_version(LONG_VERSION.as_str()))]
-pub struct CliArguments {
-    /// Add additional directories to search for fonts
-    #[clap(long = "font-path", value_name = "DIR", action = ArgAction::Append, env = "TYPST_FONT_PATHS", value_delimiter = ENV_PATH_SEP)]
-    pub font_paths: Vec<PathBuf>,
-
-    /// Root directory for your project
-    #[clap(long = "root", value_name = "DIR")]
-    pub root: Option<PathBuf>,
-
-    /// Preview mode
-    #[clap(long = "preview-mode", default_value = "document", value_name = "MODE")]
-    pub preview_mode: PreviewMode,
-
+pub struct PreviewArgs {
     /// Data plane server will bind to this address
     #[clap(
         long = "data-plane-host",
@@ -50,6 +37,21 @@ pub struct CliArguments {
     )]
     pub control_plane_host: String,
 
+    /// Only render visible part of the document. This can improve performance but still being experimental.
+    #[clap(long = "partial-rendering")]
+    pub enable_partial_rendering: bool,
+}
+
+#[derive(Debug, Clone, Parser)]
+#[clap(name = "typst-preview", author, version, about, long_version(LONG_VERSION.as_str()))]
+pub struct CliArguments {
+    #[clap(flatten)]
+    pub preview: PreviewArgs,
+
+    /// Preview mode
+    #[clap(long = "preview-mode", default_value = "document", value_name = "MODE")]
+    pub preview_mode: PreviewMode,
+
     /// Host for the preview server
     #[clap(
         long = "host",
@@ -63,9 +65,13 @@ pub struct CliArguments {
     #[clap(long = "no-open")]
     pub dont_open_in_browser: bool,
 
-    /// Only render visible part of the document. This can improve performance but still being experimental.
-    #[clap(long = "partial-rendering")]
-    pub enable_partial_rendering: bool,
+    /// Add additional directories to search for fonts
+    #[clap(long = "font-path", value_name = "DIR", action = ArgAction::Append, env = "TYPST_FONT_PATHS", value_delimiter = ENV_PATH_SEP)]
+    pub font_paths: Vec<PathBuf>,
+
+    /// Root directory for your project
+    #[clap(long = "root", value_name = "DIR")]
+    pub root: Option<PathBuf>,
 
     pub input: PathBuf,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,56 @@
-mod actor;
-mod args;
-mod debug_loc;
-mod outline;
-
-use std::collections::HashMap;
-use std::path::PathBuf;
-
 use clap::Parser;
-use futures::SinkExt;
-use hyper::http::Error;
-use hyper::service::{make_service_fn, service_fn};
 use log::{error, info};
-use serde::Deserialize;
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::WebSocketStream;
 
 use typst_ts_compiler::service::CompileDriver;
 use typst_ts_compiler::TypstSystemWorld;
 use typst_ts_core::config::CompileOpts;
 
-use crate::actor::editor::EditorActor;
-use crate::actor::typst::TypstActor;
-use crate::args::{CliArguments, PreviewMode};
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Error,
+};
 
-pub use typst_ts_compiler::service::DocToSrcJumpInfo;
+use typst_preview::{preview, CliArguments, PreviewMode, Previewer};
 
-#[derive(Debug, Deserialize)]
-pub struct ChangeCursorPositionRequest {
-    filepath: PathBuf,
-    line: usize,
-    /// fixme: character is 0-based, UTF-16 code unit.
-    /// We treat it as UTF-8 now.
-    character: usize,
-}
+pub async fn make_static_host(
+    previewer: &Previewer,
+    static_file_addr: String,
+    mode: PreviewMode,
+) -> SocketAddr {
+    let frontend_html = previewer.frontend_html(mode);
+    let make_service = make_service_fn(|_| {
+        let html = frontend_html.clone();
+        async move {
+            Ok::<_, hyper::http::Error>(service_fn(move |req| {
+                // todo: clone may not be necessary
+                let html = html.as_ref().to_owned();
+                async move {
+                    if req.uri().path() == "/" {
+                        log::info!("Serve frontend: {:?}", mode);
+                        Ok::<_, Error>(hyper::Response::new(hyper::Body::from(html)))
+                    } else {
+                        // jump to /
+                        let mut res = hyper::Response::new(hyper::Body::empty());
+                        *res.status_mut() = hyper::StatusCode::FOUND;
+                        res.headers_mut().insert(
+                            hyper::header::LOCATION,
+                            hyper::header::HeaderValue::from_static("/"),
+                        );
+                        Ok(res)
+                    }
+                }
+            }))
+        }
+    });
+    let server = hyper::Server::bind(&static_file_addr.parse().unwrap()).serve(make_service);
 
-// JSON.stringify({
-// 		'event': 'panelScrollTo',
-// 		'filepath': bindDocument.uri.fsPath,
-// 		'line': activeEditor.selection.active.line,
-// 		'character': activeEditor.selection.active.character,
-// 	})
-#[derive(Debug, Deserialize)]
-pub struct SrcToDocJumpRequest {
-    filepath: PathBuf,
-    line: usize,
-    /// fixme: character is 0-based, UTF-16 code unit.
-    /// We treat it as UTF-8 now.
-    character: usize,
-}
-
-impl SrcToDocJumpRequest {
-    pub fn to_byte_offset(&self, src: &typst::syntax::Source) -> Option<usize> {
-        src.line_column_to_byte(self.line, self.character)
+    let addr = server.local_addr();
+    if let Err(e) = server.await {
+        error!("Static file server error: {}", e);
     }
-}
 
-#[derive(Debug, Deserialize)]
-pub struct MemoryFiles {
-    files: HashMap<PathBuf, String>,
+    addr
 }
-
-#[derive(Debug, Deserialize)]
-pub struct MemoryFilesShort {
-    files: Vec<PathBuf>,
-    // mtime: Option<u64>,
-}
-
-/// If this file is not found, please refer to https://enter-tainer.github.io/typst-preview/dev.html to build the frontend.
-const HTML: &str = include_str!("../addons/vscode/out/frontend/index.html");
 
 /// Entry point.
 #[tokio::main]
@@ -86,11 +68,10 @@ async fn main() {
         .try_init();
     let arguments = CliArguments::parse();
     info!("Arguments: {:#?}", arguments);
-    let enable_partial_rendering = arguments.enable_partial_rendering;
     let entry = if arguments.input.is_absolute() {
         arguments.input.clone()
     } else {
-        std::env::current_dir().unwrap().join(arguments.input)
+        std::env::current_dir().unwrap().join(&arguments.input)
     };
     let root = if let Some(root) = &arguments.root {
         if root.is_absolute() {
@@ -105,6 +86,7 @@ async fn main() {
         error!("entry file must be in the root directory");
         std::process::exit(1);
     }
+
     let compiler_driver = {
         let world = TypstSystemWorld::new(CompileOpts {
             root_dir: root.clone(),
@@ -123,159 +105,24 @@ async fn main() {
         std::process::exit(0);
     });
 
-    // Create the world that serves sources, fonts and files.
-    let actor::typst::Channels {
-        typst_mailbox,
-        doc_watch,
-        renderer_mailbox,
-        editor_conn,
-        webview_conn: (webview_tx, _),
-    } = TypstActor::set_up_channels();
-    let typst_actor = TypstActor::new(
-        compiler_driver,
-        typst_mailbox.1,
-        doc_watch.0,
-        renderer_mailbox.0.clone(),
-        editor_conn.0.clone(),
-        webview_tx.clone(),
-    );
+    let previewer = preview(arguments.preview, compiler_driver).await;
 
-    tokio::spawn(typst_actor.run());
-
-    let (data_plane_port_tx, data_plane_port_rx) = tokio::sync::oneshot::channel();
-    let data_plane_addr = arguments.data_plane_host;
-    let data_plane_handle = {
-        let typst_tx = typst_mailbox.0.clone();
-        let webview_tx = webview_tx.clone();
-        let doc_watch_rx = doc_watch.1.clone();
-        tokio::spawn(async move {
-            // Create the event loop and TCP listener we'll accept connections on.
-            let try_socket = TcpListener::bind(&data_plane_addr).await;
-            let listener = try_socket.expect("Failed to bind");
-            info!(
-                "Data plane server listening on: {}",
-                listener.local_addr().unwrap()
-            );
-            let _ = data_plane_port_tx.send(listener.local_addr().unwrap().port());
-            while let Ok((stream, _)) = listener.accept().await {
-                let webview_tx = webview_tx.clone();
-                let webview_rx = webview_tx.subscribe();
-                let typst_tx = typst_tx.clone();
-                let doc_watch_rx = doc_watch_rx.clone();
-                let mut conn = accept_connection(stream).await;
-                if enable_partial_rendering {
-                    conn.send(Message::Binary("partial-rendering,true".into()))
-                        .await
-                        .unwrap();
-                }
-                let actor::webview::Channels { svg } =
-                    actor::webview::WebviewActor::set_up_channels();
-                let webview_actor = actor::webview::WebviewActor::new(
-                    conn,
-                    svg.1,
-                    webview_tx,
-                    webview_rx,
-                    typst_tx,
-                    renderer_mailbox.0.clone(),
-                );
-                tokio::spawn(webview_actor.run());
-                let render_actor = actor::render::RenderActor::new(
-                    renderer_mailbox.0.subscribe(),
-                    doc_watch_rx.clone(),
-                    svg.0,
-                );
-                render_actor.spawn();
-                let outline_render_actor = actor::render::OutlineRenderActor::new(
-                    renderer_mailbox.0.subscribe(),
-                    doc_watch_rx,
-                    editor_conn.0.clone(),
-                );
-                outline_render_actor.spawn();
-            }
-        })
-    };
-
-    let control_plane_addr = arguments.control_plane_host;
-    let control_plane_handle = {
-        let typst_tx = typst_mailbox.0.clone();
-        let editor_rx = editor_conn.1;
-        tokio::spawn(async move {
-            let try_socket = TcpListener::bind(&control_plane_addr).await;
-            let listener = try_socket.expect("Failed to bind");
-            info!(
-                "Control plane server listening on: {}",
-                listener.local_addr().unwrap()
-            );
-            let (stream, _) = listener.accept().await.unwrap();
-            let conn = accept_connection(stream).await;
-            let editor_actor = EditorActor::new(editor_rx, conn, typst_tx, webview_tx);
-            editor_actor.run().await;
-        })
-    };
     let static_file_addr = arguments.static_file_host;
-    let data_plane_port = data_plane_port_rx.await.unwrap();
-    let make_service = make_service_fn(|_| {
-        async move {
-            Ok::<_, hyper::http::Error>(service_fn(move |req| {
-                async move {
-                    if req.uri().path() == "/" {
-                        let html = HTML.replace(
-                            "ws://127.0.0.1:23625",
-                            format!("ws://127.0.0.1:{data_plane_port}").as_str(),
-                        );
-                        // previewMode
-                        let mode = match arguments.preview_mode {
-                            PreviewMode::Document => "Doc",
-                            PreviewMode::Slide => "Slide",
-                        };
-                        let html = html.replace(
-                            "preview-arg:previewMode:Doc",
-                            format!("preview-arg:previewMode:{}", mode).as_str(),
-                        );
-                        log::info!("Preview mode: {}", mode);
-                        Ok::<_, Error>(hyper::Response::new(hyper::Body::from(html)))
-                    } else {
-                        // jump to /
-                        let mut res = hyper::Response::new(hyper::Body::empty());
-                        *res.status_mut() = hyper::StatusCode::FOUND;
-                        res.headers_mut().insert(
-                            hyper::header::LOCATION,
-                            hyper::header::HeaderValue::from_static("/"),
-                        );
-                        Ok(res)
-                    }
-                }
-            }))
+    let mode = arguments.preview_mode;
+    if !static_file_addr.is_empty() {
+        let addr = make_static_host(&previewer, static_file_addr, mode).await;
+        info!("Static file server listening on: {}", addr);
+        if !arguments.dont_open_in_browser {
+            if let Err(e) = open::that_detached(format!("http://{}", addr)) {
+                error!("failed to open browser: {}", e);
+            };
         }
-    });
-    let server = hyper::Server::bind(&static_file_addr.parse().unwrap()).serve(make_service);
-    if !arguments.dont_open_in_browser {
-        if let Err(e) = open::that_detached(format!("http://{}", server.local_addr())) {
-            error!("failed to open browser: {}", e);
-        };
     }
-    info!("Static file server listening on: {}", server.local_addr());
-    if let Err(e) = server.await {
-        error!("Static file server error: {}", e);
-    }
-    let _ = tokio::join!(data_plane_handle, control_plane_handle);
+
+    previewer.join().await;
 }
 
-async fn accept_connection(stream: TcpStream) -> WebSocketStream<TcpStream> {
-    let addr = stream
-        .peer_addr()
-        .expect("connected streams should have a peer address");
-    info!("Peer address: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-
-    info!("New WebSocket connection: {}", addr);
-    ws_stream
-}
-
-use std::borrow::Cow;
+use std::{borrow::Cow, net::SocketAddr};
 
 pub static EMBEDDED_FONT: &[Cow<'_, [u8]>] = &[
     // Embed default fonts.
