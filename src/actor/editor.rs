98dc9bd306402c87ddfde921aab539b61e597e56
mgt <mgt@oi-wiki.org>
2024-03-10 12:42:01 +0800

    bump every thing

--- src/actor/editor.rs
+++ src/actor/editor.rs
@@ -27,7 +27,7 @@ pub struct PanelScrollByPositionRequest {
     position: DocumentPosition,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 #[serde(tag = "kind", content = "data")]
 pub enum CompileStatus {
     Compiling,
