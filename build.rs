b7db246d8a61a2f1caf9d3a6aa6b74ac68c3027c
Myriad-Dreamin <camiyoru@gmail.com>
2024-03-16 10:42:10 +0800

    dev: embed 404 frontend if not found

--- build.rs
+++ build.rs
@@ -10,5 +10,12 @@ fn main() -> Result<()> {
         .git_describe(true, true, None)
         .all_rustc()
         .emit()?;
+
+    // touch if not exists
+    const ERROR_404: &str = r#"<html lang="en"><head><meta charset="UTF-8" /><title>404 Not Found</title></head><body><h1>404 Not Found</h1><p>This typst-preview is built without frontend.</p></body></html>"#;
+    if !std::path::Path::new("addons/vscode/out/frontend/index.html").exists() {
+        std::fs::create_dir_all("addons/vscode/out/frontend")?;
+        std::fs::write("addons/vscode/out/frontend/index.html", ERROR_404)?;
+    }
     Ok(())
 }
