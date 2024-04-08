4786f0f3e0a9abb440794836aa7d1dd0fa60bd7c
mgt <mgt@oi-wiki.org>
2024-04-08 23:49:31 +0800

    chore: add proper version string

--- src/args.rs
+++ src/args.rs
@@ -110,6 +110,7 @@ pub struct CliArguments {
 pub static LONG_VERSION: Lazy<String> = Lazy::new(|| {
     format!(
         "
+Version:             {}
 Build Timestamp:     {}
 Build Git Describe:  {}
 Commit SHA:          {}
@@ -117,6 +118,7 @@ Commit Date:         {}
 Commit Branch:       {}
 Cargo Target Triple: {}
 ",
+        env!("CARGO_PKG_VERSION"),
         env!("VERGEN_BUILD_TIMESTAMP"),
         env!("VERGEN_GIT_DESCRIBE"),
         option_env!("VERGEN_GIT_SHA").unwrap_or("None"),
